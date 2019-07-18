1b372901c58e3f93b3272dfbee26e01361dcf6dc
iMaary <m.rodriguesnave@gmail.com>
2019-07-18 11:18:12 -0300

    Zoom Inicio

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class Camera : MonoBehaviour
 {
     [SerializeField] private GameObject player;
-    [SerializeField] private float camVel, limiteMin, limiteMax;
+    [SerializeField] private float camVel, limiteMin, limiteMax, zoomMax, zoomMin;
     [SerializeField] private bool segue;
 
-    void Update()
-    {
+    void Update(){
         var novaCamPos = Vector3.Lerp(transform.position, player.transform.position, camVel);
-        transform.position = new Vector3(transform.position.x, Mathf.Clamp(novaCamPos.y, limiteMin, limiteMax), transform.position.z); 
+        var posZ = Vector3.Lerp(transform.position, player.transform.position, 0.0075f);
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(novaCamPos.y, limiteMin, limiteMax), Mathf.Clamp(posZ.z, zoomMin, zoomMax)); 
     }
 }

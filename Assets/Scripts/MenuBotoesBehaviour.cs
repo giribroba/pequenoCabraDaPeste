b924292a77a5bb9e99e008b22d12a67c7eabc9b4
iMaary <m.rodriguesnave@gmail.com>
2019-07-19 10:09:02 -0300

    Menu pronto

--- Assets/Scripts/MenuBotoesBehaviour.cs
+++ Assets/Scripts/MenuBotoesBehaviour.cs
@@ -5,17 +5,22 @@ using UnityEngine.UI;
 
 public class MenuBotoesBehaviour : MonoBehaviour
 {
-    private Vector2 tamInicial;
+    [SerializeField] private Sprite botaoNovo;
+    private Sprite botaoNormal;
+    private Image img;
 
     void Start(){
-        tamInicial = transform.position;
+        img = GetComponent<Image>();
+        botaoNormal = img.sprite;
     }
 
     public void Aumenta(){
         transform.localScale *= 1.2f;
+        img.sprite = botaoNovo;
     }
 
     public void Normal(){
         transform.localScale /= 1.2f;
+        img.sprite = botaoNormal;
     }
 }

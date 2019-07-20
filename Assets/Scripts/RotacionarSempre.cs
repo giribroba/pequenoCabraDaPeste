a94b4711c9c826f388390601e6e5dcc49635b776
iMaary <m.rodriguesnave@gmail.com>
2019-07-20 14:48:11 -0300

    plataforma em zig zag

---
+++ Assets/Scripts/RotacionarSempre.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotacionarSempre : MonoBehaviour
+{
+    private GameObject planeta;
+    void Update(){
+        planeta = GameObject.FindWithTag("Redoneta");
+        transform.up = -new Vector2(planeta.transform.position.x - transform.position.x, planeta.transform.position.y - transform.position.y);
+    }
+}

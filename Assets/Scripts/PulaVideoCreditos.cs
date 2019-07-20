d38249d8f45ccf5c81cc2f9f6dab028db61b1c9b
iMaary <m.rodriguesnave@gmail.com>
2019-07-20 14:56:05 -0300

    Créditos implementados

---
+++ Assets/Scripts/PulaVideoCreditos.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PulaVideoCreditos : MonoBehaviour
+{
+    void Start(){
+        Invoke("PulaVideo", 10.5f);
+    }
+        
+    void PulaVideo(){
+        SceneManager.LoadScene("Menu");
+    }
+}

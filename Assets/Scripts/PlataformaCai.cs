29acaf8a476a80257d1048b6e50078d5f72c6fe2
Thiago Mendes <captaincezar1@gmail.com>
2019-07-25 12:25:08 -0300

    gramas / plataforma que cai

--- Assets/Scripts/PlataformaCai.cs
+++ Assets/Scripts/PlataformaCai.cs
@@ -23,7 +23,7 @@ public class PlataformaCai : MonoBehaviour
         caido = true;
         yield return new WaitForSeconds(0.2f);
         anim.SetBool("Tremendo", true);
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.3f);
         anim.SetBool("Tremendo", false);
         anim.SetBool("Caindo", true);
         yield return new WaitForSeconds(1.5f);

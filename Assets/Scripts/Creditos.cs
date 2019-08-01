1a9deb1e17e1acd92372eaaf49c19a82f0c9ee7b
giribroba <giribroba@gmail.com>
2019-08-01 09:44:25 -0300

    Novos créditos

---
+++ Assets/Scripts/Creditos.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Creditos : MonoBehaviour
+{
+    [SerializeField] float tempo;
+    void Start()
+    {
+        StartCoroutine("Fim");
+    }
+
+    private IEnumerator Fim()
+    {
+        yield return new WaitForSeconds(tempo);
+        SceneManager.LoadScene("Menu");
+    }
+}

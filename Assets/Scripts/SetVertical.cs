c524ec5e0c836e500160e1b19f40df053f793389
giribroba <giribroba>
2020-06-25 18:33:24 -0300

    botoes poço funcionando (falta ocultar corretamente)

---
+++ Assets/Scripts/SetVertical.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SetVertical : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+    private enum tipo { U, D };
+    [SerializeField] tipo botao;
+    [SerializeField] GameObject poco;
+
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (botao == tipo.U)
+        {
+            poco.GetComponent<pocoBehaviour>().vertical = 1;
+        }
+        if (botao == tipo.D)
+        {
+            poco.GetComponent<pocoBehaviour>().vertical = -1;
+        }
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        poco.GetComponent<pocoBehaviour>().vertical = 0;
+    }
+}
--- Assets/Scripts/pocoBehaviour.cs
+++ Assets/Scripts/pocoBehaviour.cs
@@ -5,12 +5,13 @@ public class pocoBehaviour : MonoBehaviour
     [SerializeField] private GameObject corda, gota, iconeBalde, interagir;
     [SerializeField] private Sprite[] sprites;
     [SerializeField] private int capacidade, indexArray, cBaldada = 0;
+    [SerializeField] private AudioSource baldeIn;
     private SpriteRenderer cRenderer;
     private bool cAgua = false, bPoco = false, interagiu = false;
     private float profundidade;
     private RaycastHit2D[] hit;
     private Animator cAnim;
-    [SerializeField] private AudioSource baldeIn;
+    public float vertical;
 
     void Start()
     {
@@ -51,15 +52,63 @@ public class pocoBehaviour : MonoBehaviour
                         bPoco = true;
                         iconeBalde.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1);
                     }
-#endif
-                    if (Input.GetKeyDown("e") && other.GetComponent<controladorJogador>().balde && !bPoco)
+                    else if (interagiu && !other.GetComponent<controladorJogador>().balde && bPoco && profundidade <= 0)
+                    {
+                        interagiu = false;
+                        if (capacidade <= 0)
+                        {
+                            this.transform.GetChild(0).gameObject.tag = "Removido";
+                            Objetivo.SetObjetivo("Poço");
+                            Feed();
+                        }
+                        bPoco = false;
+                        other.GetComponent<controladorJogador>().balde = true;
+                        iconeBalde.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0.5f);
+                        if (indexArray == 2)
+                        {
+                            other.GetComponent<controladorJogador>().aguaCantil.fillAmount += 0.15f;
+                            other.GetComponent<controladorJogador>().AddFlor();
+                            other.GetComponent<controladorJogador>().contBaldada++;
+                        }
+                    }
+                    if (bPoco)
+                    {
+
+                        if (vertical > 0)
+                        {
+                            profundidade -= (profundidade < 0) ? 0 : Time.deltaTime;
+                            cAnim.enabled = (vertical != 0 && profundidade >= 0);
+                        }
+                        else if (vertical < 0)
+                        {
+                            profundidade += (profundidade > 1) ? 0 : Time.deltaTime;
+                            cAnim.enabled = (vertical != 0 && profundidade <= 1);
+                        }
+                        else
+                        {
+                            profundidade = (Mathf.Abs(profundidade) < 0.05f) ? 0 : profundidade;
+                            cAnim.enabled = false;
+                        }
+                        if (capacidade > 0 && profundidade >= 1 && !cAgua)
+                        {
+                            baldeIn.Play();
+                            cAgua = true;
+                            capacidade--;
+                        }
+                        else if (capacidade <= 0 && profundidade <= 0.08f && !cAgua)
+                        {
+                            cAgua = false;
+                        }
+                    }
+#else
+                    if (Input.GetButtonDown("Fire1") && other.GetComponent<controladorJogador>().balde && !bPoco)
                     {
                         cAgua = false;
                         other.GetComponent<controladorJogador>().balde = false;
                         bPoco = true;
                         iconeBalde.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1);
                     }
-                    else if (Input.GetKeyDown("e") && !other.GetComponent<controladorJogador>().balde && bPoco && profundidade <= 0)
+                    else if (Input.GetButtonDown("Fire1") && !other.GetComponent<controladorJogador>().balde && bPoco && profundidade <= 0)
                     {
                         if (capacidade <= 0)
                         {
@@ -106,6 +155,7 @@ public class pocoBehaviour : MonoBehaviour
                             cAgua = false;
                         }
                     }
+#endif
                 }
             }
 
@@ -122,6 +172,7 @@ public class pocoBehaviour : MonoBehaviour
     {
         if (collision.tag == "Player")
         {
+
             gota.GetComponent<SpriteRenderer>().enabled = true;
             iconeBalde.GetComponent<SpriteRenderer>().enabled = true;
         }
@@ -130,13 +181,16 @@ public class pocoBehaviour : MonoBehaviour
     {
         if (collision.tag == "Player")
         {
+            interagir.SetActive(false);
             gota.GetComponent<SpriteRenderer>().enabled = false;
             iconeBalde.GetComponent<SpriteRenderer>().enabled = false;
+#if UNITY_STANDALONE
             if (collision.GetComponent<controladorJogador>().balde && collision.GetComponent<controladorJogador>().primeiroPoco)
             {
                 transform.GetChild(0).gameObject.SetActive(false);
                 collision.GetComponent<controladorJogador>().primeiroPoco = collision.GetComponent<controladorJogador>().contBaldada == 0;
             }
+#endif
         }
     }
     private void OnTriggerEnter2D(Collider2D col)
